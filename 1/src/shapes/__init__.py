d5796b960ff2ed7cead3a4e5eed47181eba06422
Artem Gargoma <berbi_zet@mail.ru>
2021-03-22 02:44:52 +0300

    completed

---
+++ 1/src/shapes/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from shapes.Circle import Circle
+from shapes.Ellipse import Ellipse
+from shapes.Line import Line
+from shapes.Polygon import Polygon
+from shapes.Ray import Ray
+from shapes.Segment import Segment
+from shapes.RegularPolygon import RegularPolygon
+from shapes.Rectangle import Rectangle
+from shapes.Square import Square
+from shapes.Rhombus import Rhombus
+
+shapes = [Line, Ray, Segment, Rectangle, Square, Rhombus, Circle, Ellipse, Polygon, RegularPolygon]
